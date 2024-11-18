dbb8eea39e11bdbcaeb6b7360837c31058869664
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-11-18 13:39:46 -0800

    made a fake db and a component that shows all the cards component/products

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import blank from "../assets/img/Black_White_Border_Frame_Transparent_PNG_Image.png";
 import "./Card.css";
 
-function Card() {
+function Card({ productName = "product" }) {
   return (
-    <div className="flex flex-col gap-2 justify-center h-80 shadow-xl rounded-md">
-      <h2>Product</h2>
+    <div className="flex flex-col gap-2 justify-center h-80 shadow-xl rounded-md ">
+      <h2 className="h-12">{productName}</h2>
       <img src={blank} className="card-img"></img>
       <h3>$9.99</h3>
     </div>
--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,12 +1,14 @@
 import Card from "./Card";
+import fakedb from "../fakedb.json";
 
 function ProductsContainer() {
+  // console.log(fakedb[0].name);
+
   return (
     <div className="my-8 w-2/2 grid sm:grid-cols-4 col-auto gap-6  justify-items-end">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {fakedb.map((product) => (
+        <Card key={product.id} productName={product.name} />
+      ))}
       <Card />
     </div>
   );

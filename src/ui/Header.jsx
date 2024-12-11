0ec08d7372650555cd873d3934df52f045ed9d7e
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-12-11 16:51:32 -0500

    added a todo txt and changes some style

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,9 @@
 function Header() {
-  return <div>MartialMart</div>;
+  return (
+    <div className="text-slate-50 text-2xl drop-shadow-lg font-bold">
+      MartialMart
+    </div>
+  );
 }
 
 export default Header;

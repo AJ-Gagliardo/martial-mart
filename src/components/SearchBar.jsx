5363c98b8487810ce8f5479f281b30652518c96a
Antonio Gagliardo <ajgagliardo92@gmail.com>
2025-01-04 08:39:26 -0800

    added a query state, and made it chang when the user writes something on the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,11 @@
-function SearchBar({ search, setSearch }) {
+function SearchBar({ query, setQuery }) {
   return (
     <div className="my-8 ">
       <p>Search by Name</p>
       <input
         type="text"
         className="border border-slate-600 h-8  w-2/3 shadow-md"
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button className="h-8 p-0 w-8">0</button>
     </div>
